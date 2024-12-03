e83f8db43dc0579558b058661b82bb3c9ed73093
Soon Ming Qian <145448385+SoonMingQian@users.noreply.github.com>
2024-12-03 14:33:48 +0000

    device service implementation

--- device-info-app/src/app/home/home.page.ts
+++ device-info-app/src/app/home/home.page.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardContent, IonCardTitle, IonCardSubtitle, IonCard, IonCardHeader, IonButton } from '@ionic/angular/standalone';
+import { DeviceInfoService } from '../services/device-info.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCardContent, IonCardTitle, IonCardSubtitle, IonCard, IonCardHeader, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  deviceInfo: any;
+
+  constructor(private deviceInfoService: DeviceInfoService) {}
+
+  async getDeviceInfo() {
+    this.deviceInfo = await this.deviceInfoService.getDeviceInfo();
+  }
 }
